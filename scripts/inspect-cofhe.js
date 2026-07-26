8f97c95fc0466b063d64944cbc2e54bd5291235b
Iam-jayant <jayantkurekar1@gmail.com>
2026-07-26 13:56:42 +0530

    scripts removal

--- scripts/inspect-cofhe.js
+++
@@ -1,8 +0,0 @@
-const cofheReact = require("@cofhe/react");
-const cofheSdk = require("@cofhe/sdk");
-
-console.log("=== COFHE REACT EXPORTS ===");
-console.log(Object.keys(cofheReact));
-
-console.log("\n=== COFHE SDK EXPORTS ===");
-console.log(Object.keys(cofheSdk));
